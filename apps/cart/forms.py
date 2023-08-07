43f09c5e35bcf887d677c5b8f6a67643a15470a3
Askar Social <ernur.social@bk.ru>
2023-08-07 13:04:17 +0600

    changed forms.py

--- apps/cart/forms.py
+++ apps/cart/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import OrderItem
+from apps.cart.models import OrderItem
 
 
 class OrderForm(forms.ModelForm):
